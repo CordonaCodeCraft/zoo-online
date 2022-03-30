47876db4788075b3e243300dd1920b0366e0860a
Ventsislav Stoevski <cordonahcodecraft@gmail.com>
2022-03-30 07:43:18 +0300

    Initial commit

---
+++ src/main/kotlin/tech/cordona/zooonline/ZooOnlineApplication.kt
@@ -0,0 +1,11 @@
+package tech.cordona.zooonline
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ZooOnlineApplication
+
+fun main(args: Array<String>) {
+    runApplication<ZooOnlineApplication>(*args)
+}
---
+++ src/test/kotlin/tech/cordona/zooonline/ZooOnlineApplicationTests.kt
@@ -0,0 +1,13 @@
+package tech.cordona.zooonline
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ZooOnlineApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
