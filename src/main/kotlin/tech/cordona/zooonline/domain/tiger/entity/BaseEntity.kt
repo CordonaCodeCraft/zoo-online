e28163b2bc24b34ce444d78f4f1af5236f792d4e
Ventsislav Stoevski <cordonahcodecraft@gmail.com>
2022-04-01 08:07:26 +0300

    fix: fixes wrong implementation of the BaseEntity
    Signed-off-by: Ventsislav Stoevski <cordonahcodecraft@gmail.com>

--- src/main/kotlin/tech/cordona/zooonline/domain/tiger/entity/BaseEntity.kt
+++ src/main/kotlin/tech/cordona/zooonline/domain/tiger/entity/BaseEntity.kt
@@ -4,9 +4,9 @@ import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import java.time.LocalDateTime
 
-abstract class BaseEntity
-
-@Id
-var id: ObjectId = ObjectId.get()
-var createdDate: LocalDateTime = LocalDateTime.now()
-var modifiedDate: LocalDateTime = LocalDateTime.now()
+abstract class BaseEntity (
+	@Id
+	var id: ObjectId = ObjectId.get(),
+	var createdDate: LocalDateTime = LocalDateTime.now(),
+	var modifiedDate: LocalDateTime = LocalDateTime.now()
+)
