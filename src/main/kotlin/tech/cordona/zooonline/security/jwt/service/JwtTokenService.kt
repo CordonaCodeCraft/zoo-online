399d0e58e6ed3973eff0278be768dbe5aa2a04ee
Ventsislav Stoevski <cordonahcodecraft@gmail.com>
2022-04-21 16:09:16 +0300

    chore: code clean-up

--- src/main/kotlin/tech/cordona/zooonline/security/jwt/service/JwtTokenService.kt
+++ src/main/kotlin/tech/cordona/zooonline/security/jwt/service/JwtTokenService.kt
@@ -3,6 +3,6 @@ package tech.cordona.zooonline.security.jwt.service
 import tech.cordona.zooonline.security.jwt.dto.JwtTokenInfo
 
 interface JwtTokenService {
-	fun createEmailVerificationToken(email: String, id: String) : String
+	fun createEmailVerificationToken(email: String, id: String): String
 	fun decodeToken(token: String): JwtTokenInfo
 }
